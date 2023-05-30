753fba9708b1e9302b2ba582707a67d018383f2d
bochra ss <bochra.salahsalah@gmail.com>
2023-05-30 08:16:05 +0200

    bootstrapa app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MDBBootstrapModule} from "angular-bootstrap-md";
+import { TeamComponent } from './team/team.component';
+import { PricesComponent } from './prices/prices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    PricesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
